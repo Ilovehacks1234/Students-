a5d7c8a21494203a6eda42bcf704a1240bc72edf
Ilovehacks1234 <106293178+Ilovehacks1234@users.noreply.github.com>
2026-02-21 15:54:23 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import StudMetricsWebsite from "./App";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <StudMetricsWebsite />
+  </React.StrictMode>
+);
